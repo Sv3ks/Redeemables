a00a418fbc4db9f15626a507869b002e7a7ff602
Isaac <isaactcantrell@gmail.com>
2024-08-20 18:44:55 -0500

    Added new config option: required empty slots
    various code changes

--- src/main/java/dev/sv3ks/redeemables/commands/RedeemCommand.java
+++ src/main/java/dev/sv3ks/redeemables/commands/RedeemCommand.java
@@ -1,6 +1,8 @@
 package dev.sv3ks.redeemables.commands;
 
+import dev.sv3ks.redeemables.Redeemables;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,73 +11,78 @@ import org.bukkit.entity.Player;
 import java.util.ArrayList;
 import java.util.List;
 
-import static dev.sv3ks.redeemables.Redeemables.getPlugin;
-
 public class RedeemCommand implements CommandExecutor {
+    
+    final Redeemables main;
+
+    public RedeemCommand(Redeemables main) {
+        this.main = main;
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
-        if (!(sender instanceof Player)) {
+        if (!(sender instanceof Player player)) {
             sender.sendMessage("Only players can redeem rewards!");
             return true;
         }
 
         if (args.length != 1) {
-            sender.sendMessage("§cIncorrect syntax. Correct syntax: /redeem <code>");
+            sender.sendMessage(ChatColor.RED + "Incorrect syntax. Correct syntax: /redeem <code>");
             return true;
         }
 
-        if (!getPlugin().getConfig().isSet(args[0])) {
-            sender.sendMessage("§cInvalid code.");
+        if (!main.getConfig().isSet(args[0])) {
+            sender.sendMessage(ChatColor.RED + "Invalid code.");
             return true;
         }
 
-        Player player = (Player) sender;
-
-        if (
-                !getPlugin().getConfig().isSet(args[0] + ".used_by") ||
-                !getPlugin().getConfig().getStringList(args[0] + ".used_by").contains(player.getUniqueId().toString())
-        ) {
-            List<String> used_by = new ArrayList<>();
-            if (getPlugin().getConfig().isSet(args[0] + ".used_by")) {
-                used_by = getPlugin().getConfig().getStringList(args[0] + ".used_by");
-            }
-            used_by.add(player.getUniqueId().toString());
-            getPlugin().getConfig().set(args[0] + ".used_by", used_by);
-            getPlugin().saveConfig();
-            try {
-                for (String rewardCommand :
-                        getPlugin().getConfig().getStringList(args[0] + ".commands")) {
-                    Bukkit.dispatchCommand(
-                            Bukkit.getConsoleSender(),
-                            rewardCommand.replace("%PLAYER%", player.getName())
-                    );
-                }
-            } catch (NullPointerException e) {
-                sender.sendMessage("§4Internal Error: " + args[0] + ".commands is not set.");
+        if (main.getConfig().isSet(args[0] + ".used_by")) {
+            if (main.getConfig().getStringList(args[0] + ".used_by").contains(player.getUniqueId().toString())) {
+                sender.sendMessage(ChatColor.RED + "You already used that code!");
                 return true;
             }
-            sender.sendMessage("§aYou redeemed " + args[0] + "§a!");
+            if (main.getConfig().getStringList(args[0] + ".used_by").size() == main.getConfig().getInt(args[0] + ".uses")) {
+                sender.sendMessage(ChatColor.RED + "The code has reached its max uses.");
+                return true;
+            }
+        }
+
+        List<String> used_by = new ArrayList<>();
+        if (main.getConfig().isSet(args[0] + ".used_by")) {
+            used_by = main.getConfig().getStringList(args[0] + ".used_by");
+        }
+
+        if (main.getConfig().getInt(args[0] + ".required empty slots", 0) > emptySlots(player)) {
+            player.sendMessage(ChatColor.RED + "Not enough empty inventory slots!");
             return true;
         }
 
-        if (
-                getPlugin().getConfig().getStringList(args[0] + ".used_by").contains(player.getUniqueId().toString())
-        ) {
-            sender.sendMessage("§cYou already used that code!");
+        used_by.add(player.getUniqueId().toString());
+        main.getConfig().set(args[0] + ".used_by", used_by);
+        main.saveConfig();
+        try {
+            for (String rewardCommand :
+                    main.getConfig().getStringList(args[0] + ".commands")) {
+                Bukkit.dispatchCommand(
+                        Bukkit.getConsoleSender(),
+                        rewardCommand.replace("%PLAYER%", player.getName())
+                );
+            }
+        } catch (NullPointerException e) {
+            sender.sendMessage(ChatColor.DARK_RED + "Internal Error: " + args[0] + ".commands is not set.");
             return true;
         }
-
-        if (
-                getPlugin().getConfig().getStringList(args[0] + ".used_by").size() ==
-                        getPlugin().getConfig().getInt(args[0] + ".uses")
-        ) {
-            sender.sendMessage("§cThe code has reached its max uses.");
-            return true;
-        }
-
-
-        player.sendMessage("§cI have no idea what happened.");
+        sender.sendMessage(ChatColor.GREEN + "You redeemed " + args[0] + ChatColor.GREEN + "!");
         return true;
     }
+
+    private int emptySlots(Player player) {
+        int slots = 0;
+        for (int i = 0; i < 35; i++) {
+            if (player.getInventory().getItem(i) == null)
+                slots++;
+        }
+        return slots;
+    }
+
 }
--- src/main/java/dev/sv3ks/redeemables/commands/ReloadCommand.java
+++ src/main/java/dev/sv3ks/redeemables/commands/ReloadCommand.java
@@ -1,17 +1,24 @@
 package dev.sv3ks.redeemables.commands;
 
+import dev.sv3ks.redeemables.Redeemables;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-import static dev.sv3ks.redeemables.Redeemables.getPlugin;
-
 public class ReloadCommand implements CommandExecutor {
+
+    final Redeemables main;
+
+    public ReloadCommand(Redeemables main) {
+        this.main = main;
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
-        getPlugin().reloadConfig();
-        getPlugin().saveConfig();
-        sender.sendMessage("§7Reloading codes...");
+        main.reloadConfig();
+        main.saveConfig();
+        sender.sendMessage(ChatColor.GRAY + "Reloading codes...");
         return false;
     }
 }
